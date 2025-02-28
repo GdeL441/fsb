7d771c0913d3ce7ea3a3047e52ad4376a88675c4
MatsDK <matsdekort257@gmail.com>
2025-02-28 15:41:18 +0100

    connect to wifi button

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,18 +1,22 @@
 use tauri::Manager;
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet() -> Vec<String> {
+fn scan() -> Vec<String> {
     let wifi = wifiscanner::scan().unwrap_or_default();
     println!("{wifi:?}");
     wifi.into_iter().map(|w| w.ssid).collect()
+}
+
+#[tauri::command]
+fn connect(ssid: String) {
+    println!("Connect to {ssid}");
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![scan, connect])
         .setup(|app| {
             #[cfg(debug_assertions)]
             app.get_webview_window("main").unwrap().open_devtools();
--- app/src/main.js
+++ app/src/main.js
@@ -1,17 +1,39 @@
 const { invoke } = window.__TAURI__.core;
 
-async function greet() {
-  const wifiSSIDs = await invoke("greet");
-  ssids.innerText = wifiSSIDs.join(", ")
+async function scan() {
+  loading.style.display = "block"
+  const wifiSSIDs = await invoke("scan");
+  loading.style.display = "none"
+
+  ssids.innerHTML = ""
+  for (const ssid of wifiSSIDs) {
+    const li = document.createElement("li")
+    const button = document.createElement("button")
+    const span = document.createElement("span")
+    span.innerText = `${ssid}`
+    button.innerText = "Connect"
+    button.addEventListener("click", async () => {
+      const res = await invoke("connect", { ssid })
+      console.log("connected to wifi", res)
+    });
+    li.appendChild(span)
+    li.appendChild(button)
+    ssids.appendChild(li)
+  };
 }
 
 let ssids
+let loading
 
 window.addEventListener("DOMContentLoaded", () => {
   const btn = document.querySelector("#scan-btn");
   ssids = document.querySelector("#wifi-ssids")
+
+  loading = document.querySelector("#loading")
+  loading.style.display = "none"
+
   btn.addEventListener("click", (e) => {
     e.preventDefault();
-    greet();
+    scan();
   });
 });
