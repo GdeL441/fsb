60504c076c334823a4e5f26f25c6e9f96a68e237
MatsDK <matsdekort257@gmail.com>
2025-02-28 17:32:36 +0100

    connect wifi

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,4 +1,10 @@
-use tauri::Manager;
+use std::sync::{Arc, Mutex};
+
+use tauri::{Manager, State};
+use wifi_rs::{
+    prelude::{Config, Connectivity},
+    WiFi,
+};
 
 #[tauri::command]
 fn scan() -> Vec<String> {
@@ -8,18 +14,43 @@ fn scan() -> Vec<String> {
 }
 
 #[tauri::command]
-fn connect(ssid: String) {
+fn connect(state: State<'_, AppData>, ssid: String) {
     println!("Connect to {ssid}");
+    let mut wifi = state.wifi.lock().unwrap();
+    match wifi.connect("ssid", "") {
+        Ok(result) => println!(
+            "{}",
+            if result == true {
+                "Connection Successful."
+            } else {
+                "Invalid password."
+            }
+        ),
+        Err(err) => println!("The following error occurred: {:?}", err),
+    }
+}
+
+#[derive(Clone, Debug)]
+struct AppData {
+    wifi: Arc<Mutex<WiFi>>,
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let config = Some(Config {
+        interface: Some("en0"),
+    });
+
+    let wifi = Arc::new(Mutex::new(WiFi::new(config)));
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![scan, connect])
         .setup(|app| {
             #[cfg(debug_assertions)]
             app.get_webview_window("main").unwrap().open_devtools();
+
+            app.manage(AppData { wifi });
             Ok(())
         })
         .run(tauri::generate_context!())
