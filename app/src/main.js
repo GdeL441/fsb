86a37387b913ae822a80c9d1aa1fd1fe0911edf6
MatsDK <matsdekort257@gmail.com>
2025-03-27 16:51:53 +0100

    update styles.css

--- app/src/main.js
+++ app/src/main.js
@@ -5,14 +5,18 @@ async function connectWs(url) {
   ws = new WebSocket(url);
 
   ws.onopen = () => {
-    document.getElementById("status").textContent = "Status: Connected";
+    document.getElementById("status").textContent = "Connected";
+    statusDot.classList.add("bg-success")
+    statusDot.classList.remove("bg-danger")
     console.log('WebSocket connection opened')
 
     hideConnectView()
   };
   ws.onclose = () => {
     console.log('WebSocket connection closed')
-    document.getElementById("status").textContent = "Status: Disconnected";
+    statusDot.classList.remove("bg-success")
+    statusDot.classList.add("bg-danger")
+    document.getElementById("status").textContent = "Disconnected";
     ws = null
     showConnectView()
   };
@@ -21,10 +25,10 @@ async function connectWs(url) {
     console.log("data received from websocket", data)
     if (data.action == "next_step") {
       console.log(data.step)
-      step.textContent = `Current step: ${data.step}`
+      step.textContent = `${data.step}`
     } else if (data.action == "position_updated") {
       position.textContent = JSON.stringify(data.position)
-      heading.textContent = `Heading: ${data.heading}`
+      heading.textContent = `Heading: ${data.heading} `
       console.log("heading", data.heading, "position", data.position)
       drawDot(data.position.x, data.position.y, data.heading)
     } else if (data.action == "finished") {
@@ -32,13 +36,15 @@ async function connectWs(url) {
       stopTimer()
     } else if (data.action == "sensor_values") {
       console.log("Sensor values", data)
-      sensors.textContent = `Left: ${data.L}, Right: ${data.R}, Back: ${data.B}`
+      sensors.textContent = `Left: ${data.L}, Right: ${data.R}, Back: ${data.B} `
     }
 
   };
   ws.onerror = error => {
     console.error(error)
-    document.getElementById("status").textContent = "Status: Disconnected";
+    document.getElementById("status").textContent = "Disconnected";
+    statusDot.classList.remove("bg-success")
+    statusDot.classList.add("bg-danger")
     ws = null
     showConnectView()
   };
@@ -68,7 +74,7 @@ async function scan() {
     const li = document.createElement("li")
     const button = document.createElement("button")
     const span = document.createElement("span")
-    span.innerText = `${ssid}`
+    span.innerText = `${ssid} `
     button.innerText = "Connect"
     button.addEventListener("click", async () => {
       // const _wsUrl = await invoke("connect", { ssid })
@@ -123,7 +129,7 @@ function resetTimer() {
   updateDisplay();
 }
 
-let ssids, loading, ipInput, position, heading, step, sensors
+let ssids, loading, ipInput, position, heading, step, sensors, statusDot
 
 let canvas, ctx
 const COLS = 7; // Aantal rijen en kolommen
@@ -211,10 +217,11 @@ window.addEventListener("DOMContentLoaded", () => {
 
   ssids = document.querySelector("#wifi-ssids")
   ipInput = document.querySelector("#ip-input")
-  position = document.querySelector("#pos")
-  heading = document.querySelector("#heading")
+  position = document.querySelector("#position")
+  heading = document.querySelector("#heading_")
   step = document.querySelector("#step")
   sensors = document.querySelector("#sensors")
+  statusDot = document.querySelector("#status-dot")
 
   // loading = document.querySelector("#loading")
   // loading.style.display = "none"
@@ -251,8 +258,8 @@ window.addEventListener("DOMContentLoaded", () => {
   // });
 
   startBtn.addEventListener("click", async (e) => {
-    drawDot(1, 1, "S");
-    startTimer()
+    // drawDot(1, 1, "S");
+    // startTimer()
     e.preventDefault();
     if (!ws) return
     ws.send(JSON.stringify({ action: "start" }))
@@ -260,8 +267,8 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   stopBtn.addEventListener("click", async (e) => {
-    drawDot(1, 1, "W");
-    stopTimer()
+    // drawDot(1, 1, "W");
+    // stopTimer()
     e.preventDefault();
     if (!ws) return
     ws.send(JSON.stringify({ action: "stop" }))
@@ -269,9 +276,9 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   resetBtn.addEventListener("click", async (e) => {
-    drawDot(2, 1, "E");
-    stopTimer()
-    resetTimer()
+    // drawDot(2, 1, "E");
+    // stopTimer()
+    // resetTimer()
     e.preventDefault();
     if (!ws) return
     ws.send(JSON.stringify({ action: "reset" }))
